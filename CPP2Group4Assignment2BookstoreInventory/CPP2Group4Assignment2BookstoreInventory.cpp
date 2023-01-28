7a577fb406b4d7c68d8bd2318b15cfc7301dd45d
Number2wo <112464904+Number2wo@users.noreply.github.com>
2023-01-27 21:30:48 -0500

    testing merge to test branch from ChrisR branch

--- CPP2Group4Assignment2BookstoreInventory/CPP2Group4Assignment2BookstoreInventory.cpp
+++ CPP2Group4Assignment2BookstoreInventory/CPP2Group4Assignment2BookstoreInventory.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "testing merge to test branch from ChrisR branch";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
