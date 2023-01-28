da11868aa3b2ec08dd900bc428cecca5378c46a9
Awsomeman328 <Awsoemman328@gmail.com>
2023-01-27 22:42:11 -0500

    Completed Implementing the Book Class
    For now I have made all of the properties strings, even the ISPN and the publication year. Mainly doing this since the source for these properties are going to be coming in as strings and our output onto the screen is going to be strings. That being said, we may still change these two properties to unsigned ints later.
    
    Also all of the setter functions for the properties are private, so once they are set by the constructor they cannot be changed. If we ever need to change them in the future though, we can change these functions to be public.

--- CPP2Group4Assignment2BookstoreInventory/Book.cpp
+++ CPP2Group4Assignment2BookstoreInventory/Book.cpp
@@ -1,23 +1,27 @@
 #include "Book.h"
 
-class Book
+// Since ISBN and Year are technically numbers, they could be stored as unsigned ints.
+// Possibly will add some overloaded functions to accomodate for both string and int values if we want or need to do so later.
+Book::Book(string ISBN, string Title, string Author, string Year, string Publisher)
 {
-public:
-	Book();
-	~Book();
-
-private:
-	string ISBN;
-	string title;
-	string suthor;
-	string year;
-	string publisher;
-};
-
-Book::Book()
-{
+	SetISBN(ISBN);
+	SetTitle(Title);
+	SetAuthor(Author);
+	SetYear(Year);
+	SetPublisher(Publisher);
 }
 
-Book::~Book()
-{
-}
+// Public functions
+string Book::GetISBN() { return _isbn; }
+string Book::GetTitle() { return _title; }
+string Book::GetAuthor() { return _author; }
+string Book::GetYear() { return _year; }
+string Book::GetPublisher() { return _publisher; }
+
+// Private functions 
+// Once a book object's properties have been set from its constructor, those properties cannot be changed or set again. This may be changed later if needed.
+void Book::SetISBN(string newISBN) { _isbn = newISBN; }
+void Book::SetTitle(string newTitle) { _title = newTitle; }
+void Book::SetAuthor(string newAuthor) { _author = newAuthor; }
+void Book::SetYear(string newYear) { _year = newYear; }
+void Book::SetPublisher(string newPublisher) { _publisher = newPublisher; }
--- CPP2Group4Assignment2BookstoreInventory/Book.h
+++ CPP2Group4Assignment2BookstoreInventory/Book.h
@@ -3,6 +3,24 @@
 using namespace std;
 class Book
 {
+public:
+	Book(string ISBN, string Title, string Author, string Year, string Publisher);
+	string GetISBN();
+	string GetTitle();
+	string GetAuthor();
+	string GetYear();
+	string GetPublisher();
 
+private:
+	string _isbn; // Could be an unsigned int, but since we're getting this info from a string format we'll start it as a string for now. Might change this later.
+	string _title;
+	string _author;
+	string _year; // Could be an unsigned int, but since we're getting this info from a string format we'll start it as a string for now. Might change this later.
+	string _publisher;
+	void SetISBN(string newISBN);
+	void SetTitle(string newTitle);
+	void SetAuthor(string newAuthor);
+	void SetYear(string newYear);
+	void SetPublisher(string newPublisher);
 };
 
