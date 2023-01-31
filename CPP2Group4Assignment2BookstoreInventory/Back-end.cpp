1c3a680c976ce80b6f27c60496a06a166ac56f76
Awsomeman328 <Awsoemman328@gmail.com>
2023-01-31 12:18:09 -0500

    Saving my Changes from Debugging Chris's Branch

--- CPP2Group4Assignment2BookstoreInventory/Back-end.cpp
+++ CPP2Group4Assignment2BookstoreInventory/Back-end.cpp
@@ -2,3 +2,37 @@
 
 using namespace std;
 
+
+	int numRows = doc.GetRowCount();
+	int numCols = doc.GetColumnCount();
+
+	vector<Book> books;
+
+	for (int i = 0; i < 100; i++) {
+		Book b;
+		b.setISBN(doc.GetRowName(i));
+		b.setTitle(doc.GetCell<string>("Book-Title", i));
+		b.setAuthor(doc.GetCell<string>("Book-Author", i));
+		b.setYear(doc.GetCell<string>("Year-Of-Publication", i));
+		b.setPublisher(doc.GetCell<string>("Publisher", i));
+		books.push_back(b);
+	}
+
+	return books;
+}
+
+int main() {
+	string fileName = "..\\books.csv";
+	vector<Book> books = readData(fileName);
+
+	cout << "Book Inventory:" << endl;
+	for (int i = 0; i < books.size(); i++) {
+		cout << "Title: " << books[i].getTitle() << endl;
+		cout << "Author: " << books[i].getAuthor() << endl;
+		cout << "Publisher: " << books[i].getPublisher() << endl;
+		cout << "Year: " << books[i].getYear() << endl;
+		cout << "ISBN: " << books[i].getISBN() << endl;
+		cout << endl;
+	}
+	return 0;
+}
