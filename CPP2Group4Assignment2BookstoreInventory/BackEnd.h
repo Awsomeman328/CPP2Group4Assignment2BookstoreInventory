dd300aed4b3dbe74dbad62ef4e7c113abdef06cd
Mike Dolan <101299595+ultador@users.noreply.github.com>
2023-02-14 20:12:53 -0500

    Update csv fields and misc changes

--- CPP2Group4Assignment2BookstoreInventory/BackEnd.h
+++ CPP2Group4Assignment2BookstoreInventory/BackEnd.h
@@ -9,3 +9,5 @@ bool checkUserPassPair(string username, string password);
 vector<Book> searchBooksByTitle(string bookTitleToSearch, size_t maxResults);
 vector<Book> searchBooksByTitle(string bookTitleToSearch, unsigned int startingIndex, size_t maxResults);
 vector<Book> searchBooksByTitle(string bookTitleToSearch, Book bookToStartFrom, size_t maxResults);
+void addBookToInventory(string ISBN, string bookTitle, string bookAuthor, string pubYear, string publisher);
+void addBookToInventory(string ISBN, string bookTitle, string bookAuthor, string pubYear, string publisher, string description, string genre);
--- CPP2Group4Assignment2BookstoreInventory/CPP2Group4Assignment2BookstoreInventory.cpp
+++ CPP2Group4Assignment2BookstoreInventory/CPP2Group4Assignment2BookstoreInventory.cpp
@@ -1,4 +1,3 @@
-#include <Windows.h>
 #include <iostream>
 #include <string>
 #include <vector>
