33c4721bfbe1d8855507fc6b1ac1b586b1bdbbdd
Mike Dolan <101299595+ultador@users.noreply.github.com>
2023-03-22 17:57:45 -0400

    Splash Screen

--- BSI/main.cpp
+++ BSI/main.cpp
@@ -2,13 +2,21 @@
 #include "utilities.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     outputToLogFile("main() Starting Program, ...");
 
     QApplication a(argc, argv);
+
+    QSplashScreen *splash= new QSplashScreen;
+    splash->setPixmap(QPixmap("scrollRackSplash.png"));
+    splash->show();
+
     MainWindow w;
-    w.show();
+    QTimer::singleShot(2500, splash, SLOT(close()));
+    QTimer::singleShot(2500, &w, SLOT(show()));
     return a.exec();
 }
