be39838b8fc81f40b71abd9ba2a67badb80a7971
Mike Dolan <101299595+ultador@users.noreply.github.com>
2023-04-18 22:15:48 -0400

    Login Screen Mk.5

--- BSI/mainwindow.cpp
+++ BSI/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    loginDialog* login;
+    loginDialog* login = new loginDialog;
 
 
     // Create the menu bar and menus
